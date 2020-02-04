4d4046384a619b2a8c0801ced08a6a119ae134db
chan0021 <54650667+chan0021@users.noreply.github.com>
2020-02-04 12:31:12 -0600

    f

---
+++ Participation/WPF Classes/EntryForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+              
+        }
+        public EntryForm(string name, string address, int zipcode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipcode;
+        }
+        public EntryForm(string address, int zipCode)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Name : { Name} Address : {Address} ZipCode : {ZipCode}";
+        }
+    }
+}
---
+++ Participation/WPF Classes/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtAddress.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtZipCode.Text = string.Empty;
+
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string name, address, zipcode;
+            name = txtName.Text;
+            address = txtAddress.Text;
+            zipcode = txtZipCode.Text;
+
+            if(string.IsNullOrWhiteSpace(address)==true)
+            {
+                MessageBox.Show("Please enter an address");
+            }
+            int Zip;
+            bool isZipAnInt = int.TryParse(zipcode, out Zip);
+            if(isZipAnInt == false)
+            {
+                MessageBox.Show("Please enter a valid address");
+            }
+
+            EntryForm ef = new EntryForm(name, address, Zip);
+            ef.Address = address;
+            ef.Name = name;
+            ef.ZipCode = Convert.ToInt32(zipcode);
+            lbxListBox.Items.Add(ef);
+        }
+    }
+}
