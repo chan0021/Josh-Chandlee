2b1637c583eecff8b1dccbe6f4a7af785380c1bd
chan0021 <54650667+chan0021@users.noreply.github.com>
2020-02-11 13:20:39 -0600

    rick and morty

---
+++ WpfParticipation/WpfParticipation/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfParticipation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, MouseEventArgs e)
+        {
+            btnSubmit.Margin = new Thickness(50,50,0,0);
+        }
+    }
+}
